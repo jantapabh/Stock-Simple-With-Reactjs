8c6bdc7df23ed0cdd6769f80ad50a77261897203
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-29 00:01:46 +0700

    add commit

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
@@ -9,14 +9,15 @@ import firebase from 'firebase'
 import { BrowserRouter, Route, Link, Router, Redirect } from 'react-router-dom';
 import Login from './components/Login'
 import Topbar from './components/Topbar'
+import Home from './components/Home'
+import Stock from './components/Stock'
+
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       user: {}
-
     }
   }
 
@@ -32,24 +33,18 @@ class App extends Component {
       else {
         this.setState({ user: null })
       }
-
     })
   }
 
   render() {
-
-    if(this.state.user == null)
-    {
-      return(
-
+    if (this.state.user == null) {
+      return (
         <div>
-        <Topbar />
-        <Login />   
+          <Login />
         </div>
-
       );
     }
-  
+
     return (
 
       <div>
@@ -57,7 +52,10 @@ class App extends Component {
           <Topbar />
         </div>
         <BrowserRouter>
-        
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/FormInput" component={FormInput} />
+          <Route path="/ListItems" component={ListItems} /> */}
+          <Route path="/login" component={Login} />
         </BrowserRouter>
       </div>
 
--- flashcard/src/components/Topbar.js
+++ flashcard/src/components/Topbar.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, FormControl, Button, Form } from 'react-bootstrap';
-
+import fire from '../config/fire';
+import Login from './Login'
+import Home from './Home'
+import Stock from './Stock'
 
 const Topbar = () => {
+
+const logout = e => {
+    fire.auth().signOut();
+}
+
     return (
         <div>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="/Home">Navbar</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="/Home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-info">Search</Button>
+                </Form >
+                <Form inline>
+                    <Button variant="warning" style={{ margin: 10 }} onClick={logout}>LOGOUT</Button>
                 </Form>
             </Navbar>
         </div>
