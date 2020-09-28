e227919aef33481f008cd1f4002bf6b115f0452f
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-28 23:45:55 +0700

    add commit

--- flashcard/src/components/Login.js
+++ flashcard/src/components/Login.js
@@ -1,140 +1,189 @@
-import React, { Component, useState, useEffect } from 'react'
-import './Login.css'
+//Use Material UI
+import React, { Component, useState, useEffect} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import fire from '../config/fire';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import firebase from 'firebase'
 import { firestore } from '../index'
 
 
-
-//Login and Sign up with firebase email and password
-const Login = () => {
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [isSignedIn, setIsSignedIn] = useState(false)
-
- const uiConfig = {
-
-        signInFlow: "popup",
-        signInOptions: [
-
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID
-        ],
-
-        callback: {
-            signInSuccess: () => false
-        }
-    }
-
-    const componentDidMount = () => {
-        firebase.auth().onAuthStateChanged(user => {
-            this.setState({ isSignedIn: !!user })
-        })
-    }
-
-    const login = e => {
-        e.preventDefault();
-        fire.auth().signInWithEmailAndPassword(email, password).then((u) =>{
-            console.log(u)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-
-
-    const signup = e => {
-        e.preventDefault()
-        fire.auth().createUserWithEmailAndPassword(email, password).then((u) => {
-            console.log(u)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-
-    return (
-        <Grid container component="main" className="root">
-                    <CssBaseline />
-                    <Grid item xs={false} sm={4} md={7} className="image" />
-                    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                        <div className="paper">
-                            <Avatar className="avata">
-                                <LockOutlinedIcon style={{ backgroundColor: 'green' }} />
-                            </Avatar>
-                            <Typography component="h1" variant="h5">
-                                Log In
-          </Typography>
-                            <form className="form" noValidate>
-                                <TextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    autoFocus
-                                    onChange={(e)=> setEmail(e.target.value)}
-                                />
-                                <TextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                    onChange={(e)=> setPassword(e.target.value)}
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox value="remember" color="primary" />}
-                                    label="Remember me"
-                                />
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary"
-                                    className="submit"
-                                    onClick={login}
-                                >
-                                    Log In
-            </Button>
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="secondary"
-                                    className="submit"
-                                    onClick={signup}
-                                    style={{ margin: 5, }}
-                                >
-                                    Sign Up
-            </Button>
-            <StyledFirebaseAuth
-                                    uiConfig={uiConfig}
-                                    firebaseAuth={firebase.auth()}
-                                />
-                            </form>
-                        </div>
-                    </Grid>
-                </Grid>
-    )
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Login = () => {
+
+  const classes = useStyles();
+
+  //Login
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
+  const uiConfig = {
+
+    signInFlow: "popup",
+    signInOptions: [
+
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+
+    callback: {
+
+        signInSuccess: () => false
+    }
+}
+
+const componentDidMount = () => {
+
+  
+
+    firebase.auth().onAuthStateChanged(user => {
+        this.setState({ isSignedIn: !!user })
+    })
+}
+
+const login = e => {
+
+    e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(email, password).then((u) =>{
+        console.log(u)
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+
+const signup = e => {
+
+    e.preventDefault()
+    fire.auth().createUserWithEmailAndPassword(email, password).then((u) => {
+        console.log(u)
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e)=> setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e)=> setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={login}
+          >
+            Sign In
+          </Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={signup}
+          >
+            Sign Up
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
 export default Login
